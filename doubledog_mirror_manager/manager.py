ac5cf59e0e1d2a6d3034ba740a4fad5beb20cd52
John Florian <jflorian@doubledog.org>
2010-06-01 16:23:18 -0400

    Change - add license headers to prominent files
    This is in preparation for initial public release.

--- doubledog_mirror_manager/manager.py
+++ doubledog_mirror_manager/manager.py
@@ -1,7 +1,21 @@
-# doubledog.org local mirror manager
+# Copyright 2009, 2010 John Florian <jflorian@doubledog.org>
+#
+# This file is part of doubledog-mirror-manager.
 # 
-# Copyright 2009, John Florian
+# doubledog-mirror-manager is free software: you can redistribute it
+# and/or modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version so long as this
+# copyright notice remains intact.
 # 
+# doubledog-mirror-manager is distributed in the hope that it will be
+# useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with doubledog-mirror-manager.  If not, see
+# <http://www.gnu.org/licenses/>.
 
 
 __author__ = """John Florian <jflorian@doubledog.org>"""
