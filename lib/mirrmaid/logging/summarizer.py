d5ce137e81100402af8ae9ced04c95212162f222
John Florian <jflorian@doubledog.org>
2018-11-16 15:48:14 -0500

    Janitorial - abbreviate/ensure license in headers
    Signed-off-by: John Florian <jflorian@doubledog.org>

--- lib/mirrmaid/logging/summarizer.py
+++ lib/mirrmaid/logging/summarizer.py
@@ -1,21 +1,9 @@
 # coding=utf-8
-# Copyright 2012-2016 John Florian <jflorian@doubledog.org>
+
+# SPDX-License-Identifier: GPL-3.0-or-later
+# Copyright 2012-2018 John Florian <jflorian@doubledog.org>
 #
 # This file is part of mirrmaid.
-#
-# mirrmaid is free software: you can redistribute it and/or modify it
-# under the terms of the GNU General Public License as published by the
-# Free Software Foundation, either version 3 of the License, or (at your
-# option) any later version so long as this copyright notice remains
-# intact.
-#
-# mirrmaid is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with mirrmaid.  If not, see <http://www.gnu.org/licenses/>.
 
 
 """
@@ -36,7 +24,7 @@ from doubledog.mail import MiniMailer
 from mirrmaid.constants import *
 
 __author__ = """John Florian <jflorian@doubledog.org>"""
-__copyright__ = """Copyright 2012-2016 John Florian"""
+__copyright__ = """Copyright 2012-2018 John Florian"""
 
 
 class SummaryGroup(object):
